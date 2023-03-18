bfa6dbf7a68f1ca943bafa7ea44c7fb0eba139c0
unknown <njorogekamau63@gmail.com>
2023-03-18 12:54:20 +0300

    :

--- ask.py
+++ ask.py
@@ -1,18 +1,27 @@
-from langchain.llms import OpenAI
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.text_splitter import CharacterTextSplitter
+from langchain import OpenAI
 from gpt_index import SimpleDirectoryReader, GPTListIndex, readers, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
 import sys
 import os
-from IPython.display import Markdown, display
+from IPython.display import Markdown, dip
+
 
 
 #construct index function
 
+from llama_index import SimpleDirectoryReader, GPTListIndex, readers, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
+from langchain import OpenAI
+import sys
+import os
+from IPython.display import Markdown, display
+
 def construct_index(directory_path):
+    # set maximum input size
     max_input_size = 4096
+    # set number of output tokens
     num_outputs = 2000
+    # set maximum chunk overlap
     max_chunk_overlap = 20
+    # set chunk size limit
     chunk_size_limit = 600 
 
     # define LLM
@@ -31,7 +40,15 @@ def construct_index(directory_path):
 
 def ask_ai():
     index = GPTSimpleVectorIndex.load_from_disk('index.json')
-    while True: 
+    while True:
         query = input("What do you want to ask? ")
         response = index.query(query, response_mode="compact")
         display(Markdown(f"Response: <b>{response.response}</b>"))
+  
+
+os.environ["OPENAI_API_KEY"] = ""
+
+
+construct_index("/chunk_txt")
+
+ask_ai()
