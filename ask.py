691593ed8a9106239f7c5bb068d4039149decf40
unknown <njorogekamau63@gmail.com>
2023-03-18 20:09:33 +0300

    the new installs and imports

--- ask.py
+++ ask.py
@@ -1,1 +1,8 @@
 from langchain.document_loaders import DirectoryLoader
+from langchain.vectorstores import Chroma
+from langchain.text_splitter import CharacterTextSplitter
+from langchain import OpenAI, VectorDBQA
+from langchain.embeddings.openai import OpenAIEmbeddings
+import magic
+import os
+import nltk
