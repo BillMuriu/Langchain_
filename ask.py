15ea0155657e93595825d1b8e8f40c2915b2642d
unknown <njorogekamau63@gmail.com>
2023-03-19 13:23:55 +0300

    testing the doument loader

--- ask.py
+++ ask.py
@@ -7,7 +7,7 @@ import magic
 import os
 import nltk
 
-os.environ['OPENAI_API_KEY'] =''
+os.environ['OPENAI_API_KEY'] ='
 
 loader = DirectoryLoader('/chunk_txt', glob='**/*.txt')
 
@@ -15,18 +15,20 @@ loader = DirectoryLoader('/chunk_txt', glob='**/*.txt')
 documents = loader.load()
 
 
-text_splitter = CharacterTextSplitter(chunk_size=1000, overlap=0)
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 
 
-texts = text_splitter.split_documents(documents)
+# texts = text_splitter.split_documents(documents)
 
 
-embeddings = OpenAIEmbeddings(openai_api_key=os.environ['OPENAI_API_KEY'])
+# embeddings = OpenAIEmbeddings(openai_api_key=os.environ['OPENAI_API_KEY'])
 
 
-docsearch = Chroma.from_documents(texts, embeddings)
+# docsearch = Chroma.from_documents(texts, embeddings)
 
 
-qa = VectorDBQA.from_chain_type(llm=OpenAI(), chain_type="stuff", vectorstore=docsearch)
+# qa = VectorDBQA.from_chain_type(llm=OpenAI(), chain_type="stuff", vectorstore=docsearch)
 
 
+# query = "What is Naval Ravikant's Story?"
+# qa.run(query)
