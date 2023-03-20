9fc3b9e96d1a4d5ff0d263b73e42c3accdabeea4
unknown <njorogekamau63@gmail.com>
2023-03-20 10:58:43 +0300

    ask bot function

--- ask.py
+++ ask.py
@@ -5,13 +5,28 @@ import os
 
 os.environ["OPENAI_API_KEY"] = ''
 
-def construct_index():
+def construct_index(directory_path):
     max_input_size = 4096
     num_outputs = 256
     max_chunk_overlap = 20
     chunk_size_limit = 600
 
 
-    prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit)
+    prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
 
+    llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-ada-001", max_tokens=num_outputs))
+  
+    documents = SimpleDirectoryReader(directory_path).load_data()
     
+    index = GPTSimpleVectorIndex(documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+    
+    index.save_to_disk('index.json')
+    
+    return index
+
+def ask_bot(input_index = 'index.json'):
+  index = GPTSimpleVectorIndex.load_from_disk(input_index)
+  while True:
+    query = input('What do you want to ask the bot?   \n')
+    response = index.query(query, response_mode="compact")
+    print ("\nBot says: \n\n" + response.response + "\n\n\n")
