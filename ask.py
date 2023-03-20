b07f70269b1a4fcf223bda16cbdab58c7d041523
unknown <njorogekamau63@gmail.com>
2023-03-20 11:26:19 +0300

    I deleted the chunks

--- ask.py
+++ ask.py
@@ -10,23 +10,22 @@ def construct_index(directory_path):
     num_outputs = 256
     max_chunk_overlap = 20
     chunk_size_limit = 600
-
-
     prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
-
     llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-ada-001", max_tokens=num_outputs))
-  
     documents = SimpleDirectoryReader(directory_path).load_data()
-    
     index = GPTSimpleVectorIndex(documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper)
-    
     index.save_to_disk('index.json')
-    
     return index
 
-def ask_bot(input_index = 'index.json'):
-  index = GPTSimpleVectorIndex.load_from_disk(input_index)
-  while True:
-    query = input('What do you want to ask the bot?   \n')
-    response = index.query(query, response_mode="compact")
-    print ("\nBot says: \n\n" + response.response + "\n\n\n")
+def ask_bot(input_index='index.json'):
+    index = GPTSimpleVectorIndex.load_from_disk(input_index)
+    while True:
+        query = input('What do you want to ask the bot?   \n')
+        response = index.query(query, response_mode="compact")
+        if response.response is not None:
+            print("\nBot says: \n\n" + response.response + "\n\n\n")
+        else:
+            print("\nBot says: \n\n" + "I'm sorry, I don't have an answer for that. Please try another question.\n\n\n")
+
+index = construct_index("\chunk_txt")
+ask_bot('index.json')
