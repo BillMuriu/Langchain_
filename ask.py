2be6075d86ab8227d063f95fae79a6e1748d5992
unknown <njorogekamau63@gmail.com>
2023-04-12 14:40:16 +0300

    Install and import pinecone

--- ask.py
+++ ask.py
@@ -1,9 +1,16 @@
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import UnstructuredPDFLoader, OnlinePDFLoader
+from langchain.vectorstores import Pinecone
+import pinecone
 import os
 import nltk
 import magic
+
+with open('openaiapikey.txt', 'r') as f:
+    api_key = f.read().strip()
+    os.environ["OPENAI_API_KEY"] = api_key
+
 
 loader = UnstructuredPDFLoader("sample.pdf")
 
@@ -12,9 +19,10 @@ data = loader.load()
 print (f'You have {len(data)} document(s) in your data')
 print(f'There are {len(data[0].page_content)} characters in your document')
 
-# os.environ["OPENAI_API_KEY"] 
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 texts = text_splitter.split_documents(data)
 
 print (f'Now you have {len(texts)} documents')
+
+embeddings = OpenAIEmbeddings(openai_api_key=os.environ["OPENAI_API_KEY"])
