8d7017e24b9c0afda7e5dade38f7f2d566d337f8
unknown <njorogekamau63@gmail.com>
2023-04-12 22:46:52 +0300

    The QA works

--- ask.py
+++ ask.py
@@ -2,6 +2,8 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import UnstructuredPDFLoader, OnlinePDFLoader
 from langchain.vectorstores import Pinecone
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
 import pinecone
 import os
 import nltk
@@ -20,7 +22,7 @@ print (f'You have {len(data)} document(s) in your data')
 print(f'There are {len(data[0].page_content)} characters in your document')
 
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=0)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 texts = text_splitter.split_documents(data)
 
 print (f'Now you have {len(texts)} documents')
@@ -30,16 +32,20 @@ embeddings = OpenAIEmbeddings(openai_api_key=os.environ["OPENAI_API_KEY"])
 
 # initialize pinecone
 
-pinecone.init(
-    
-)
+
 
 index_name = "first-index"
 
 docsearch = Pinecone.from_texts([t.page_content for t in texts], embeddings, index_name=index_name)
 
+
+llm = OpenAI (temperature=0, openai_api_key=os.environ["OPENAI_API_KEY"])
+chain = load_qa_chain(llm, chain_type="stuff")
+
 query = "What is Specific Knowledge?"
 docs = docsearch.similarity_search(query, include_metadata=True)
 
-for doc in docs:
-    print(doc.page_content.encode('utf-8'))
+
+chain.run(input_documents=docs, question=query)
+
+print(chain.run(input_documents=docs, question=query))
