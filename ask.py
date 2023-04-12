84cd97dbbfd86c2010cc051e72d8bdcb662067ce
unknown <njorogekamau63@gmail.com>
2023-04-12 21:58:39 +0300

    add utf-8 encoding before printing the results

--- ask.py
+++ ask.py
@@ -20,9 +20,26 @@ print (f'You have {len(data)} document(s) in your data')
 print(f'There are {len(data[0].page_content)} characters in your document')
 
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=0)
 texts = text_splitter.split_documents(data)
 
 print (f'Now you have {len(texts)} documents')
 
 embeddings = OpenAIEmbeddings(openai_api_key=os.environ["OPENAI_API_KEY"])
+
+
+# initialize pinecone
+
+pinecone.init(
+    
+)
+
+index_name = "first-index"
+
+docsearch = Pinecone.from_texts([t.page_content for t in texts], embeddings, index_name=index_name)
+
+query = "What is Specific Knowledge?"
+docs = docsearch.similarity_search(query, include_metadata=True)
+
+for doc in docs:
+    print(doc.page_content.encode('utf-8'))
