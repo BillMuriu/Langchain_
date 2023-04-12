c2d7abd7e1357b301324014afeeea06db7976854
unknown <njorogekamau63@gmail.com>
2023-04-12 14:21:22 +0300

    load the documents from my local files

--- ask.py
+++ ask.py
@@ -1,33 +1,15 @@
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
-from langchain.text_splitter import CharacterTextSplitter
-from langchain import OpenAI, VectorDBQA
-from langchain.document_loaders import DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import UnstructuredPDFLoader, OnlinePDFLoader
 import os
 import nltk
 import magic
 
-os.environ["OPENAI_API_KEY"] = 
+loader = UnstructuredPDFLoader("sample.pdf")
 
-loader = DirectoryLoader('chunk_txt/', glob='**/*.txt')
-docs = loader.load()
+data = loader.load()
 
-char_text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap = 0)
+print (f'You have {len(data)} document(s) in your data')
+print (f'There are {len(data(0).page_content)} characters in your document')
 
-doc_chunks = char_text_splitter.split_documents(docs)
-
-print(doc_chunks)
-
-openAI_embeddings = OpenAIEmbeddings(openai_api_key=os.environ["OPENAI_API_KEY"])
-
-embeddings = openAI_embeddings.embed_documents(texts=doc_chunks)
-
-qa = VectorDBQA.from_chain_type(llm=OpenAI(), chain_type="stuff", vectorstore=docsearch)
-
-# Extract only the embedded vectors from each document
-vector_list = []
-for doc_vectors in embeddings:
-    for vector in doc_vectors:
-        vector_list.append(vector)
-
-vstore = Chroma.from_vectors(vector_list)
+# os.environ["OPENAI_API_KEY"] 
