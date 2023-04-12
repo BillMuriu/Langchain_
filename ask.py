3251e367dbf8d1d984d8dc7d506a7085e68a9605
unknown <njorogekamau63@gmail.com>
2023-04-12 14:28:57 +0300

    split the pdf into chunks

--- ask.py
+++ ask.py
@@ -10,6 +10,11 @@ loader = UnstructuredPDFLoader("sample.pdf")
 data = loader.load()
 
 print (f'You have {len(data)} document(s) in your data')
-print (f'There are {len(data(0).page_content)} characters in your document')
+print(f'There are {len(data[0].page_content)} characters in your document')
 
 # os.environ["OPENAI_API_KEY"] 
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+texts = text_splitter.split_documents(data)
+
+print (f'Now you have {len(texts)} documents')
