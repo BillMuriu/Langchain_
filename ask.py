8d1d22b1bf111f235924763035cb8024c7ef0b82
unknown <njorogekamau63@gmail.com>
2023-04-16 12:54:55 +0300

    create an embedding for each chunk

--- ask.py
+++ ask.py
@@ -10,10 +10,18 @@ import nltk
 import magic
 import json
 import uuid
+import openai
 
 with open('openaiapikey.txt', 'r') as f:
     api_key = f.read().strip()
     os.environ["OPENAI_API_KEY"] = api_key
+
+def open_file(filepath):
+    with open(filepath, 'r', encoding='utf-8') as infile:
+        return infile.read()
+
+
+openai.api_key = open_file('openaiapikey.txt')
 
 
 loader = UnstructuredPDFLoader("sample.pdf")
@@ -29,32 +37,29 @@ texts = text_splitter.split_documents(data)
 
 print (f'Now you have {len(texts)} documents')
 
-
-chunks = []
-for i, text in enumerate(texts):
-    chunk_id = str(uuid.uuid4())
-    chunk = {
-        "id": chunk_id,
-        "text": text
-    }
-    chunks.append(chunk)
-
-
-for i, chunk in enumerate(chunks):
-    if i < 3:
-        print(f"Chunk ID: {i+1}")
-        print(chunk)
+def gpt3_embedding(content, engine='text-embedding-ada-002'):
+    content = content.encode(encoding='ASCII',errors='ignore').decode()  # fix any UNICODE errors
+    response = openai.Embedding.create(input=content,engine=engine)
+    vector = response['data'][0]['embedding']  # this is a normal list
+    return vector
 
 
 # create a list to hold the chunk dictionaries
 chunk_dicts = []
+
+
 for i, text in enumerate(texts):
     chunk_id = str(uuid.uuid4())
+    chunk_text = text['page_content']
+    embedding = gpt3_embedding(chunk_text)
     chunk = {
         "id": chunk_id,
-        "text": text
+        "text": chunk_text,
+        "embedding": embedding
     }
     chunk_dicts.append(chunk)
+
+
 
 with open("chunks.json", "w") as f:
     for chunk_dict in chunk_dicts:
