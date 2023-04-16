30f0ceb5c6273bbaab5a0301b4f7ec3c3fd4cb42
unknown <njorogekamau63@gmail.com>
2023-04-16 13:23:40 +0300

    create a file populate.py and load the json files

---
+++ populate.py
@@ -0,0 +1,23 @@
+import pinecone
+import json
+
+pinecone.init(
+    api_key = "0c54847e-ce3a-4eef-9d26-e19e6f93a35c",
+    environment = "asia-southeast1-gcp"
+)
+
+index_name = "first-index"
+
+
+index = pinecone.Index("first-index")
+
+with open("chunks.json", "r") as f:
+    chunks = [json.loads(line) for line in f]
+
+for i in range(3):
+    chunk = chunks[i]
+    print(f"Chunk {i + 1}")
+    print(f"ID: {chunk['id']}")
+    print(f"Text: {chunk['text']}")
+    print(f"Embedding: {chunk['embedding']}")
+    print()
